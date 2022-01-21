ed40501160bd3e42ba03cea3f41bfb9d6a1169fa
yashhramani <ramaniyash19@gmail.com>
2022-01-21 11:39:15 +0530

    admin site customization added

--- restro/admin.py
+++ restro/admin.py
@@ -2,10 +2,32 @@ from django.contrib import admin
 from .models import Customer, Punjabi, Gujarati, South_Indian, Italian
 
 # Register your models here.
-admin.site.register(Punjabi)
-admin.site.register(Gujarati)
-admin.site.register(South_Indian)
-admin.site.register(Italian)
+admin.site.site_header = 'Restro Administration'
+
+class punjabiAdmin(admin.ModelAdmin):
+    list_display = ['Sabji_Name', 'Price']
+    search_fields = ['Sabji_Name']
+
+class gujaratiAdmin(admin.ModelAdmin):
+    list_display = ['Sabji_Name', 'Price']
+    search_fields = ['Sabji_Name']
+
+class SouthIndianAdmin(admin.ModelAdmin):
+    list_display = ['Name', 'Price']
+    search_fields = ['Name']
+
+class ItalianAdmin(admin.ModelAdmin):
+    list_display = ['Name', 'Price']
+    search_fields = ['Name']
+
+class customerAdmin(admin.ModelAdmin):
+    list_display = ['Name']
+    search_fields = ['Name']
+
+admin.site.register(Punjabi, punjabiAdmin)
+admin.site.register(Gujarati, gujaratiAdmin)
+admin.site.register(South_Indian, SouthIndianAdmin)
+admin.site.register(Italian, ItalianAdmin)
 # admin.site.register(Menu)
-admin.site.register(Customer)
+admin.site.register(Customer, customerAdmin)
 # admin.site.register(Billing)
